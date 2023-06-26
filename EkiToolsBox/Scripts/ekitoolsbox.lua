9d734732aa3581ad10bae945bab5a9c5bf73d423
Ekibunnel <7073622+Ekibunnel@users.noreply.github.com>
2023-06-26 18:46:58 +0200

    v0.0.2
    add ekitoolsbox command get get basic info about the mod

---
+++ EkiToolsBox/Scripts/ekitoolsbox.lua
@@ -0,0 +1,36 @@
+local version = "0.0.2"
+
+function Usage()
+	Log("Usage : ekitoolsbox [option]\n  option :  [-v | version] [-cmd | command]")
+end
+
+function CustomCmd()
+	Log("Commands:\n   dumpitemscodename\n   giveallitems\n   giveitem\n   giveweapon")
+end
+
+function EkiToolsBoxVersion()
+	Log("EkiToolsBox version "..version)
+end
+
+RegisterConsoleCommandHandler("ekitoolsbox", function(FullCommand, Parameters, Ar)
+	GlobalAr = Ar
+    if #Parameters > 1 then
+		Log(string.format("Too many Params ! err : %s", Parameters[2]))
+		Usage()
+		return false
+	end
+
+    local option = Parameters[1]
+
+	if option == "-v" or option == "version" then
+		EkiToolsBoxVersion()
+	elseif option == "-cmd" or option == "command" then
+		CustomCmd()
+	else
+		EkiToolsBoxVersion()
+		Usage()
+		CustomCmd()
+    end
+    return true
+end)
+print("[LiesofP EkiToolsBox] ekitoolsbox cmd setup\n")
